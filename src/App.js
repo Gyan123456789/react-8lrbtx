21cce1f94465bfc5973a8a1fb835141b9bdc522a
Gyan123456789 <gyanpadole84@gmail.com>
2022-02-09 23:21:59 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+// import "./style.css";
+import { add, sub, mult, div } from './Add';
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <>
+      <ul>
+        <li>{add(10, 20)}</li>
+        <li>{sub(20, 30)}</li>
+        <li>{mult(10, 20)}</li>
+        <li>{div(10, 20)}</li>
+      </ul>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,21 +4,13 @@ import ReactDOM from 'react-dom';
 // import App from "./App";
 // import Heading  from "./Heading";
  // import "./style.css";
-  import { add, sub, mult , div  } from "./Add";
+  import App from "./App";
   
 
   ReactDOM.render(
     <>
-    <ul>
-    <li>sum of Two Number:{ add(10,20) }</li>
-    <li> subtract of two Number:{ sub( 20,10)}</li>
-    <li>multilpication of Two Numbers :{ 
-    mult(10,10) }</li>
-    <li>Division of Two Number:{ div(10,3)}</li>
-
-    </ul>
-    
-    </>, 
+    <App/>
+  </>, 
     document.getElementById("root")
 
 
